e5a9055f1c3cf135216b8504c72f8ec230792fb6
sibo hao <nielhaochina@gmail.com>
2020-10-22 22:54:43 +0200

    start kf

--- src/VisualizeData/SimpleKalman.py
+++ src/VisualizeData/SimpleKalman.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+from filterpy.kalman import KalmanFilter
 from mpl_toolkits.mplot3d import Axes3D
+
 x = []
 y = []
 z = []
+
+x_kf = []
+y_kf = []
+z_kf = []
+
+kf = KalmanFilter (dim_x=2, dim_z=1)
 
 data = open("simple01.txt")
 
@@ -20,11 +30,12 @@ for line in data.readlines():
         y.append(floatLine[2])
         z.append(floatLine[3])
     
-#ax.plot(x,y,z)
-ax.scatter(x,y,z)
+ax.plot(x,y,z,c = 'green')
+ax.scatter(x,y,z,c = 'red')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
+
 
 plt.show()
 data.close()
