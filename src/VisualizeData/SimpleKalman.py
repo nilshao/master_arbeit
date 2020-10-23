55589159f2e2802fe1810594ca500d985a0eb736
sibo hao <nielhaochina@gmail.com>
2020-10-23 15:08:27 +0200

    simple kf need tuning

--- src/VisualizeData/SimpleKalman.py
+++ src/VisualizeData/SimpleKalman.py
@@ -7,6 +7,7 @@ from filterpy.kalman import KalmanFilter
 from mpl_toolkits.mplot3d import Axes3D
 from filterpy.common import Q_discrete_white_noise
 
+# append x,y,z original and x,y,z after filtering in order to visualize later
 x_ori = []
 y_ori = []
 z_ori = []
@@ -15,10 +16,13 @@ x_kf = []
 y_kf = []
 z_kf = []
 
+# define kalman filter: x is condition vector, z is observation vector
 kf = KalmanFilter (dim_x = 6, dim_z = 3)
 
+# read the file
 data = open("simple01.txt")
 
+#initialize plotting
 fig = plt.figure()
 ax = Axes3D(fig)
 
@@ -34,20 +38,23 @@ kf.H = np.array([[1.,0.,0.,0.,0.,0.],
                 [0.,1.,0.,0.,0.,0.],
                 [0.,0.,1.,0.,0.,0.]])
 #kf covariance matrix: P already contains np.eye, multiplies uncertainty
-kf.P *= 00
+kf.P *= 10
 #measurement noice:
-kf.R = np.array([[00.,00.,00.],
-                [00.,00.,00.],
-                [00.,00.,00.]])
+kf.R = np.array([[5.,5.,5.],
+                [5.,5.,5.],
+                [5.,5.,5.]])
 
-#process noise
-#https://filterpy.readthedocs.io/en/latest/common/common.html
+#process noise         https://filterpy.readthedocs.io/en/latest/common/common.html
 kf.Q = Q_discrete_white_noise(dim = 2, dt=0.03, var=1., block_size=3)
 
 for line in data.readlines():
+
+    # split the information by space
     curline = line.strip().split(" ")
+    # and save the information in a map
     floatLine = map(float,curline)
     
+    #if there is coordinate in this line
     if len(floatLine)>1:
 
         #kf initialization
@@ -60,6 +67,7 @@ for line in data.readlines():
             kf.x = np.array([[x_kf[0]],[y_kf[0]],[z_kf[0]],[0.],[0.],[0.]])
 
         else:
+            #zt is the observation vector
             zt = np.array([[floatLine[1]],[floatLine[2]],[floatLine[3]]])
             kf.predict()
             kf.update(zt)
@@ -72,12 +80,14 @@ for line in data.readlines():
         y_ori.append(floatLine[2])
         z_ori.append(floatLine[3])  
     
+#plot is a line, scatter are discrete points
 ax.plot(x_ori,y_ori,z_ori,c = 'green')
 ax.scatter(x_ori,y_ori,z_ori,c = 'green')
 
 ax.plot(x_kf,y_kf,z_kf,c = 'red')
 ax.scatter(x_kf,y_kf,z_kf,c = 'red')
 
+#add lable
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
