904f3ece3eaa4509deaeb8c2635969ade6b7c3d0
sibo hao <nielhaochina@gmail.com>
2020-10-23 14:47:39 +0200

    easy kalman

--- src/VisualizeData/SimpleKalman.py
+++ src/VisualizeData/SimpleKalman.py
@@ -5,33 +5,79 @@ import numpy as np
 
 from filterpy.kalman import KalmanFilter
 from mpl_toolkits.mplot3d import Axes3D
+from filterpy.common import Q_discrete_white_noise
 
-x = []
-y = []
-z = []
+x_ori = []
+y_ori = []
+z_ori = []
 
 x_kf = []
 y_kf = []
 z_kf = []
 
-kf = KalmanFilter (dim_x=2, dim_z=1)
+kf = KalmanFilter (dim_x = 6, dim_z = 3)
 
 data = open("simple01.txt")
 
 fig = plt.figure()
 ax = Axes3D(fig)
 
+#kf state transition matrix
+kf.F = np.array([[1.,0.,0.,1.,0.,0.],
+                [0.,1.,0.,0.,1.,0.],
+                [0.,0.,1.,0.,0.,1.],
+                [0.,0.,0.,1.,0.,0.],
+                [0.,0.,0.,0.,1.,0.],
+                [0.,0.,0.,0.,0.,1.]])
+#kf measurement function:
+kf.H = np.array([[1.,0.,0.,0.,0.,0.],
+                [0.,1.,0.,0.,0.,0.],
+                [0.,0.,1.,0.,0.,0.]])
+#kf covariance matrix: P already contains np.eye, multiplies uncertainty
+kf.P *= 00
+#measurement noice:
+kf.R = np.array([[00.,00.,00.],
+                [00.,00.,00.],
+                [00.,00.,00.]])
+
+#process noise
+#https://filterpy.readthedocs.io/en/latest/common/common.html
+kf.Q = Q_discrete_white_noise(dim = 2, dt=0.03, var=1., block_size=3)
+
 for line in data.readlines():
     curline = line.strip().split(" ")
-    
     floatLine = map(float,curline)
-    if len(floatLine)>1:
-        x.append(floatLine[1])
-        y.append(floatLine[2])
-        z.append(floatLine[3])
     
-ax.plot(x,y,z,c = 'green')
-ax.scatter(x,y,z,c = 'red')
+    if len(floatLine)>1:
+
+        #kf initialization
+        if len(x_kf) == 0:
+            x_kf.append(floatLine[1])
+            y_kf.append(floatLine[2])
+            z_kf.append(floatLine[3])
+            
+            #xt = [Tx,Ty,Tz,Tx.dot,Ty.dot,Tz.dot]
+            kf.x = np.array([[x_kf[0]],[y_kf[0]],[z_kf[0]],[0.],[0.],[0.]])
+
+        else:
+            zt = np.array([[floatLine[1]],[floatLine[2]],[floatLine[3]]])
+            kf.predict()
+            kf.update(zt)
+
+            x_kf.append(kf.x[0])
+            y_kf.append(kf.x[1])
+            z_kf.append(kf.x[2])
+
+        x_ori.append(floatLine[1])
+        y_ori.append(floatLine[2])
+        z_ori.append(floatLine[3])  
+    
+ax.plot(x_ori,y_ori,z_ori,c = 'green')
+ax.scatter(x_ori,y_ori,z_ori,c = 'green')
+
+ax.plot(x_kf,y_kf,z_kf,c = 'red')
+ax.scatter(x_kf,y_kf,z_kf,c = 'red')
+
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
