d046f365ee592571d0bd3304e84283c6920a53f6
nilshao <nielhaochina@gmail.com>
2020-11-10 16:08:51 +0100

    1110

---
+++ src/info_dictionary/src/CalculateUsingTf.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python2.7
+'''
+                                        formal programm V2.0
+        in this script, i calculate the matrix using kinematic chain.
+        with get TF function!!
+'''
+import rospy
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+import tf
+import math
+import thread
+import time
+import message_filters
+
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+from cv_bridge import CvBridge, CvBridgeError
+from tf.transformations import quaternion_matrix
+from tf import TransformListener
+from cv_bridge import CvBridge, CvBridgeError
+
+from tf2_msgs.msg import TFMessage
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import PoseStamped
+from franka_msgs.msg import FrankaState
+
+from rospy import init_node, is_shutdown
+
+# Constant parameters used in Aruco methods
+ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+'''
+ARUCO_DICTIONARY = aruco.Dictionary_get(aruco.DICT_5X5_100)
+ARUCO_SIZE_METER = 0.0996
+'''
+ARUCO_DICTIONARY = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+ARUCO_SIZE_METER = 0.0834
+# Create vectors we'll be using for rotations and translations for postures
+rvec, tvec = None, None
+
+# checked by rostopiecho /rgb/camera_info
+# azure: 606... checked!
+# distortion coefficients from camera calibration
+matrix_coefficients = np.array([np.array([606.6464233398438, 0.0, 639.0460205078125]),
+                                np.array([0.0, 606.6519775390625, 368.244140625]),
+                                np.array([0.0, 0.0, 1.0])])
+distortion_coefficients = np.array(
+    [0.5164358615875244, -2.606694221496582, 0.00045736812171526253, -0.00019684531434904784,
+     1.499117374420166, 0.39795395731925964, -2.4385111331939697, 1.4303737878799438])
+
+# initialize for writing information
+ee_to_base = Pose()
+marker_to_c = Pose()
+
+camera_to_base_pose_stamped = PoseStamped()
+base_camera_marker_stamped = PoseStamped()
+ee_to_base_pose_stamped = PoseStamped()
+
+initial_pose_found = False
+marker_to_camera_dic = {}
+
+def image_callback(img_msg):
+    bridge = CvBridge()
+    cv_image = bridge.imgmsg_to_cv2(img_msg, "passthrough")
+
+    pic_ori = cv_image
+    pic_gray = cv2.cvtColor(pic_ori, cv2.COLOR_BGR2GRAY)
+
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_100)  # Use 5x5 dictionary to find markers
+    parameters = aruco.DetectorParameters_create()  # Marker detection parameters
+
+    # lists of ids and the corners beloning to each id
+
+    corners, ids, rejected_img_points = aruco.detectMarkers(pic_gray, ARUCO_DICTIONARY, parameters=parameters)
+    global marker_to_camera_dic
+    marker_to_camera_dic = {}
+    aruco.drawDetectedMarkers(pic_gray, corners)
+
+    if np.all(ids is not None):
+        # First initialize a PoseArry message
+        pose_information = PoseArray()
+        pose_information.header.frame_id = "rgb_camera_link"
+        pose_information.header.stamp = rospy.Time.now()
+
+        num_of_markers = ids.size
+        res = aruco.estimatePoseSingleMarkers(corners, ARUCO_SIZE_METER, (matrix_coefficients),
+                                              (distortion_coefficients))
+        rvec = res[0]
+        tvec = res[1]
+        #  markerPoints=res[2]
+
+        aruco.drawDetectedMarkers(pic_gray, corners)  # Draw A square around the markers
+        marker_output_msg = ()
+        for i in range(0, ids.size):  # Iterate in markers
+            # Estimate pose of each marker and return the values rvec and tvec---different from camera coefficients
+            aruco.drawAxis(pic_gray, matrix_coefficients, distortion_coefficients, rvec[i][0], tvec[i][0], 0.1)
+
+            # we need a homogeneous transformation_matrix but OpenCV only gives us a 3x3 rotation transformation_matrix
+            rotation_matrix = np.array([[0, 0, 0, 0],
+                                        [0, 0, 0, 0],
+                                        [0, 0, 0, 0],
+                                        [0, 0, 0, 1]],
+                                       dtype=float)
+            rotation_matrix[:3, :3], _ = cv2.Rodrigues(rvec[i][0])
+
+            rotation_matrix[0][3] = tvec[i][0][0]
+            rotation_matrix[1][3] = tvec[i][0][1]
+            rotation_matrix[2][3] = tvec[i][0][2]
+            marker_to_camera_dic[ids[i][0]] = rotation_matrix
+
+            # convert the transformation_matrix to a quaternion
+            quaternion = tf.transformations.quaternion_from_matrix(rotation_matrix)
+
+            marker_to_c.position.x = float(tvec[i][0][0])
+            marker_to_c.position.y = float(tvec[i][0][1])
+            marker_to_c.position.z = float(tvec[i][0][2])
+
+            marker_to_c.orientation.x = quaternion[0]
+            marker_to_c.orientation.y = quaternion[1]
+            marker_to_c.orientation.z = quaternion[2]
+            marker_to_c.orientation.w = quaternion[3]
+
+            pose_information.poses.append(marker_to_c)
+            marker_single_info = (ids[i][0],
+                                  marker_to_c.orientation.x, marker_to_c.orientation.y, marker_to_c.orientation.z,
+                                  marker_to_c.orientation.w,
+                                  marker_to_c.position.x, marker_to_c.position.y, marker_to_c.position.z)
+            marker_output_msg = marker_output_msg + (marker_single_info,)
+     #   print(marker_to_camera_dic)
+        cv2.imshow("Gray Image Window", pic_gray)
+        cv2.waitKey(1)
+        #      print(marker_to_camera_dic)
+        # publish to the topic
+        pub = rospy.Publisher('MarkerPositionPublishing', PoseArray, queue_size=1)
+        rate = rospy.Rate(30)  # Hz
+        pub.publish(pose_information)
+        rate.sleep()
+
+def tf_to_dic_func(tf_listener):
+    (position, quaternion) = tf_listener.lookupTransform("/panda_link0", "/panda_link8", rospy.Time(0))
+    transformation_matrix = quaternion_matrix(quaternion)
+
+    # transformation
+    transformation_matrix[0][3] = position[0]
+    transformation_matrix[1][3] = position[1]
+    transformation_matrix[2][3] = position[2]
+    dict_here = {}
+    dict_here[582] = transformation_matrix
+    return dict_here
+
+def calibration_func(marker_to_joint_dic, marker_to_camera_dic, joint_to_base_dic):
+    # result also save in a dictionary
+    res = {}
+    if len(marker_to_camera_dic) == 0:
+        return "i cannot see any marker now"
+
+    # iterate all the keys in the marker to camera matrix
+    for i in marker_to_camera_dic:
+
+        try:
+            # kinematic chain
+            T_marker_to_camera = marker_to_camera_dic[i]
+            T_joint_to_base = joint_to_base_dic[i]
+            T_marker_to_ee = marker_to_joint_dic[i]
+            T_marker_to_camera_inv = np.linalg.inv(T_marker_to_camera)
+
+            # get camera to base
+            res_tmp = ((T_joint_to_base).dot(T_marker_to_ee)).dot(T_marker_to_camera_inv)
+            res[i] = res_tmp
+
+        except:
+            print("arguments is still not enough!")
+
+    return res
+
+def calculate_marker_to_joint():
+    transformation_matrix = quaternion_matrix([-0.653, 0.271, -0.271, 0.653])
+
+    # transformation
+    transformation_matrix[0][3] = 0.055
+    transformation_matrix[1][3] = 0.055
+    transformation_matrix[2][3] = 0.052
+
+    # save the info in matrix
+    dict_here = {}
+    dict_here[582] = transformation_matrix
+    return dict_here
+
+def MatrixToPoseStamped(matrix):
+
+    pose_part = Pose()
+    quaternion = tf.transformations.quaternion_from_matrix(matrix)
+    pose_part.position.x = matrix[0][3]
+    pose_part.position.y = matrix[1][3]
+    pose_part.position.z = matrix[2][3]
+    pose_part.orientation.x = quaternion[0]
+    pose_part.orientation.y = quaternion[1]
+    pose_part.orientation.z = quaternion[2]
+    pose_part.orientation.w = quaternion[3]
+    return pose_part
+
+def calibration():
+    rospy.init_node('online_calibration')
+    sub_image = rospy.Subscriber("/rgb/image_raw", Image, image_callback)
+
+    marker_to_joint_dic = calculate_marker_to_joint()
+
+    tf_listener = tf.TransformListener()
+    rate = rospy.Rate(5.0)
+
+    camera_to_base_pose_stamped.header.frame_id = "panda_link0"
+    camera_to_base_pose_stamped.header.stamp = rospy.Time.now()
+
+    while not rospy.is_shutdown():
+        try:
+            joint_to_base_dic = tf_to_dic_func(tf_listener)
+
+            calibrated_cam_to_base = calibration_func(marker_to_joint_dic, marker_to_camera_dic, joint_to_base_dic)
+            print(calibrated_cam_to_base)
+            camera_to_base_pose_stamped.pose = MatrixToPoseStamped(calibrated_cam_to_base[582])
+            pub1 = rospy.Publisher('CameraToBase', PoseStamped, queue_size=1)
+            rate = rospy.Rate(30)  # Hz
+            pub1.publish(camera_to_base_pose_stamped)
+        except:
+            print("Error!")
+            continue
+        rate.sleep()
+    rospy.spin()
+
+if __name__ == '__main__':
+    calibration()
